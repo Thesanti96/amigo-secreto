86e6ee065e37699ea714872daf24f599404f94a9
Thesanti96 <149538311+Thesanti96@users.noreply.github.com>
2023-11-20 11:27:14 -0300

    Add files via upload

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,60 @@
+let amigos = [];
+
+function adicionar(){
+    let nomeAmigo = document.getElementById('nome-amigo');
+    if (nomeAmigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+    if(amigos.includes(nomeAmigo.value)){
+        alert('Nome já incluido');
+        nomeAmigo.value = '';
+        return;
+    }
+    let listaAmigos = document.getElementById('lista-amigos');
+
+    amigos.push(nomeAmigo.value);
+
+    if (listaAmigos.textContent == '') {
+        listaAmigos.textContent = nomeAmigo.value;
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + nomeAmigo.value;
+    }
+
+    nomeAmigo.value = '';
+
+}
+
+function sortear() {
+    if (amigos.length < 4){
+        alert('Adicione no minimo 4 amigos!');
+        return;
+    }
+    embaralhar(amigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+    }
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('nome-amigo').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+    document.getElementById('lista-amigos').innerHTML = '';
+
+
+}
